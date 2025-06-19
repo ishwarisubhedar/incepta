12635269533887181ac4309e09380bff013af353
Disha Gor <dishagor2006@gmail.com>
2025-06-19 20:17:39 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
